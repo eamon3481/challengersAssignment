6f21ff73b41b3d16c74a2e6a1e3ae63d6361dd5d
Dongmin Ahn <eamon3481@naver.com>
2022-06-16 01:42:02 +0900

    👔 Add useQuery remove mocks

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,135 +1,47 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {FlatList, FlatListProps} from 'react-native';
+import {FlatList, FlatListProps, Text} from 'react-native';
+import {useQuery} from 'react-query';
 import styled from 'styled-components/native';
 
+import {challengeAPI} from '../api';
 import ChallengeItem from '../components/ChallengeItem';
 import {ChallengeItemType} from '../types/challengeItem';
 
-const mockData: ChallengeItemType[] = [
-  {
-    category: '환경',
-    endDate: '2022-06-19T23:59:59',
-    id: 198575,
-    registerCount: 7765,
-    registerEndDate: '2022-06-19T22:59:59',
-    registerStartDate: '2022-05-30T17:00:00',
-    startDate: '2022-05-30T00:00:00',
-    thumbnailImageUrl:
-      'https://d246jgzr1jye8u.cloudfront.net/development/admin/1654930852895.jpg',
-    title: '[#GREEN YOUR DAY] 환경 보호 행동 실천하고 공유하기 ',
-    type: 'official',
-  },
-  {
-    category: '생활',
-    endDate: '2022-06-24T23:59:59',
-    id: 197861,
-    registerCount: 6590,
-    registerEndDate: '2022-06-12T23:59:59',
-    registerStartDate: '2022-05-30T00:00:00',
-    startDate: '2022-06-13T00:00:00',
-    thumbnailImageUrl:
-      'https://d246jgzr1jye8u.cloudfront.net/development/admin/1644761320879.png',
-    title: '[이벤트] 미라클모닝 챌린지',
-    type: 'official',
-  },
-  {
-    category: '환경',
-    company: {
-      id: 198,
-      name: 'BGF리테일',
-    },
-    endDate: '2022-06-30T23:59:59',
-    id: 198409,
-    registerCount: 4335,
-    registerEndDate: '2022-06-13T23:59:59',
-    registerStartDate: '2022-05-26T19:00:00',
-    startDate: '2022-06-06T00:00:00',
-    thumbnailImageUrl:
-      'https://d246jgzr1jye8u.cloudfront.net/development/admin/1653895539894.jpg',
-    title: '[CU] 함께 만들어요! 대한민국 플로깅 지도',
-    type: 'partner',
-  },
-  {
-    category: '운동',
-    endDate: '2022-07-03T23:59:59',
-    id: 199466,
-    registerCount: 1702,
-    registerEndDate: '2022-06-21T23:59:59',
-    registerStartDate: '2022-06-07T00:00:00',
-    startDate: '2022-06-20T00:00:00',
-    thumbnailImageUrl:
-      'https://d246jgzr1jye8u.cloudfront.net/development/admin/1647265087794.png',
-    title: '[이벤트] 1만보 걷기 ',
-    type: 'official',
-  },
-  {
-    category: '운동',
-    endDate: '2022-07-03T23:59:59',
-    id: 199466,
-    registerCount: 1702,
-    registerEndDate: '2022-06-21T23:59:59',
-    registerStartDate: '2022-06-07T00:00:00',
-    startDate: '2022-06-20T00:00:00',
-    thumbnailImageUrl:
-      'https://d246jgzr1jye8u.cloudfront.net/development/admin/1647265087794.png',
-    title: '[이벤트] 1만보 걷기 ',
-    type: 'official',
-  },
-  {
-    category: '운동',
-    endDate: '2022-07-03T23:59:59',
-    id: 199466,
-    registerCount: 1702,
-    registerEndDate: '2022-06-21T23:59:59',
-    registerStartDate: '2022-06-07T00:00:00',
-    startDate: '2022-06-20T00:00:00',
-    thumbnailImageUrl:
-      'https://d246jgzr1jye8u.cloudfront.net/development/admin/1647265087794.png',
-    title: '[이벤트] 1만보 걷기 ',
-    type: 'official',
-  },
-  {
-    category: '운동',
-    endDate: '2022-07-03T23:59:59',
-    id: 199466,
-    registerCount: 1702,
-    registerEndDate: '2022-06-21T23:59:59',
-    registerStartDate: '2022-06-07T00:00:00',
-    startDate: '2022-06-20T00:00:00',
-    thumbnailImageUrl:
-      'https://d246jgzr1jye8u.cloudfront.net/development/admin/1647265087794.png',
-    title: '[이벤트] 1만보 걷기 ',
-    type: 'official',
-  },
-];
-
-const DATA = mockData;
 const HomeScreen: React.FC = () => {
   const [containerWidth, setContainerWidth] = useState(0);
 
   const gap = 12;
   const numColumns = 2;
 
+  const {isLoading, data} = useQuery(
+    'challengeList',
+    challengeAPI.reqChallengeList,
+  );
+  if (isLoading) {
+    return <Text>로딩</Text>;
+  }
   return (
     <HomeScreenWrapper>
-      <HomeFlatList
-        numColumns={numColumns}
-        showsVerticalScrollIndicator={false}
-        columnWrapperStyle={{
-          justifyContent: 'space-between',
-          marginBottom: gap,
-        }}
-        data={DATA}
-        onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
-        renderItem={({item}) => (
-          <ChallengeItem
-            itemWidth={(containerWidth - gap) / numColumns}
-            {...item}
-          />
-        )}
-        keyExtractor={item => item.id.toString()}
-      />
+      {data && (
+        <HomeFlatList
+          numColumns={numColumns}
+          showsVerticalScrollIndicator={false}
+          columnWrapperStyle={{
+            justifyContent: 'space-between',
+            marginBottom: gap,
+          }}
+          data={data.data.data.challenges}
+          onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
+          renderItem={({item}) => (
+            <ChallengeItem
+              itemWidth={(containerWidth - gap) / numColumns}
+              {...item}
+            />
+          )}
+          keyExtractor={item => item.id.toString()}
+        />
+      )}
     </HomeScreenWrapper>
   );
 };
