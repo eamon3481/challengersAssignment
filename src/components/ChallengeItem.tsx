f4cbd169fc7aecaaf143ede1d02c0fa00e377181
Dongmin Ahn <eamon3481@naver.com>
2022-06-15 10:45:36 +0900

    ♻️  refactor : useGetFixedRatioImageHightByWidth 를 이용해서 로직 분리

--- src/components/ChallengeItem.tsx
+++ src/components/ChallengeItem.tsx
@@ -1,31 +1,20 @@
-import React, {useState} from 'react';
-import {Text, Image, View} from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
 import styled from 'styled-components/native';
 
 import ChallengersLogo from '../assets/icons/ChallengersLogo';
+import useGetFixedRatioImageHightByWidth from '../hooks/useGetFixedRatioImageHightByWidth';
 import Tag from './Tag';
 
-// type CategoryType = '운동' | '식습관' | '생활' | '정서' | '취미' | '환경';
-
-// type responsePartnerType = {
-//   type: 'partner';
-//   company: {
-//     id: number;
-//     name: string;
-//   };
-//   category: string;
-//   registerCount: number;
-//   registerEndDate: string;
-//   registerStartDate: string;
-//   endDate: string;
-//   startDate: string;
-//   title: string;
-//   thumbnailImageUrl: string;
-//   id: number;
-// };
-export type responseOfficialType = {
-  type: string;
-  category: string;
+type CategoryType = '운동' | '식습관' | '생활' | '정서' | '취미' | '환경';
+type CompanyType = {
+  id: number;
+  name: string;
+};
+type ResponsePartnerType = {
+  type: 'partner';
+  company: CompanyType;
+  category: CategoryType;
   registerCount: number;
   registerEndDate: string;
   registerStartDate: string;
@@ -34,22 +23,36 @@ export type responseOfficialType = {
   title: string;
   thumbnailImageUrl: string;
   id: number;
-  itemWidth: number;
+};
+export type ResponseOfficialType = {
+  type: 'official';
+  category: CategoryType;
+  registerCount: number;
+  registerEndDate: string;
+  registerStartDate: string;
+  endDate: string;
+  startDate: string;
+  title: string;
+  thumbnailImageUrl: string;
+  id: number;
 };
 
-const ChallengeItem: React.FC<responseOfficialType> = ({
+export type ChallengeItemType = ResponseOfficialType | ResponsePartnerType;
+
+const ChallengeItem: React.FC<ChallengeItemType & {itemWidth: number}> = ({
   thumbnailImageUrl,
   title,
   itemWidth,
 }) => {
-  const [height, setHeight] = useState(0);
-  Image.getSize(thumbnailImageUrl, (w, h) => {
-    setHeight(h * (itemWidth / w));
+  const ImgHeight = useGetFixedRatioImageHightByWidth({
+    imgUrl: thumbnailImageUrl,
+    width: itemWidth,
   });
+
   return (
     <ChallengeItemWrapper itemWidth={itemWidth}>
       <ChallengeItemImage
-        source={{uri: thumbnailImageUrl, height}}
+        source={{uri: thumbnailImageUrl, height: ImgHeight}}
         resizeMode="contain"
       />
       <ChallengeItemTextWrapper>
@@ -71,14 +74,10 @@ export default ChallengeItem;
 
 const ChallengeItemWrapper = styled.View<{itemWidth: number}>`
   width: ${({itemWidth}) => itemWidth.toString() + 'px'};
-  border: 1px solid black;
-  border-radius: 10px;
 `;
 
 const ChallengeItemImage = styled.Image`
-  border: 1px solid black;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
+  border-radius: 10px;
 `;
 
 const ChallengeItemTextWrapper = styled.View`
