db1abb147f2fff3ec619b1bc6591f7a64ef73928
Dongmin Ahn <eamon3481@naver.com>
2022-06-16 14:51:45 +0900

    💄 Add SafeAreaView at CartScreen [#3]

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import styled from 'styled-components/native';
 
 import CartList from '../components/Cart/CartList';
 
 const CartScreen: React.FC = () => {
-  return <CartList />;
+  return (
+    <CartScreenWrapper edges={['bottom', 'left', 'right']}>
+      <CartList />
+    </CartScreenWrapper>
+  );
 };
 
 export default CartScreen;
+
+const CartScreenWrapper = styled(SafeAreaView)`
+  flex: 1;
+`;
