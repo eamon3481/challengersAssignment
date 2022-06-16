4dca6fd0e8205447f5146fb93fd6022df80998a4
Dongmin Ahn <eamon3481@naver.com>
2022-06-16 13:33:53 +0900

    💄 Style : CartList

---
+++ src/components/Cart/CartList.tsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {FlatList, FlatListProps} from 'react-native';
+import {useSelector} from 'react-redux';
+import styled from 'styled-components/native';
+
+import {useGetQueryState} from '../../hooks/useQueryClientStore';
+import {RootState} from '../../store';
+import {ChallengeItemType} from '../../types/challengeItem';
+import ChallengeItem from '../ChallengeItem';
+
+const CartList: React.FC = () => {
+  const [containerWidth, setContainerWidth] = useState(0);
+  const challengeList = useGetQueryState<ChallengeItemType[]>([
+    'challengeList',
+    undefined,
+  ]);
+
+  function trim<T>(v: T | undefined): v is T {
+    return v !== undefined;
+  }
+
+  const cartItems = useSelector<RootState, ChallengeItemType[] | undefined>(
+    state =>
+      challengeList
+        ? state.cartItems.value
+            .map(id => challengeList.find(item => item.id === id))
+            .filter(trim)
+        : undefined,
+  );
+  const gap = 0;
+  const numColumns = 1;
+
+  return (
+    <>
+      {cartItems && (
+        <ChallengeFlatList
+          numColumns={numColumns}
+          showsVerticalScrollIndicator={false}
+          data={cartItems}
+          onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
+          renderItem={({item}) => (
+            <ChallengeItem
+              isCart={false}
+              itemWidth={(containerWidth - gap) / numColumns}
+              {...item}
+            />
+          )}
+          keyExtractor={item => item.id.toString()}
+        />
+      )}
+    </>
+  );
+};
+
+const ChallengeFlatList = styled(
+  FlatList as new (
+    props: FlatListProps<ChallengeItemType>,
+  ) => FlatList<ChallengeItemType>,
+)`
+  flex: 1;
+  margin: 12px 24px;
+  height: 100%;
+`;
+
+export default CartList;
--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import {useSelector} from 'react-redux';
 
-import {useGetQueryState} from '../hooks/useQueryClientStore';
-import {RootState} from '../store';
-import {ChallengeItemType} from '../types/challengeItem';
+import CartList from '../components/Cart/CartList';
 
 const CartScreen: React.FC = () => {
-  const challengeList = useGetQueryState<ChallengeItemType[]>([
-    'challengeList',
-    undefined,
-  ]);
-
-  const cartItems = useSelector<RootState>(state =>
-    state.cartItems.value.map(id =>
-      challengeList ? challengeList.find(item => item.id === id) : [],
-    ),
-  );
-  console.log(cartItems);
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>카트</Text>
-    </View>
-  );
+  return <CartList />;
 };
 
 export default CartScreen;
